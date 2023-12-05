a0dffde2c064e475ac11ec52367bd9d1c5a6999f
Medine Burunkaya <77451962+medinebkaya@users.noreply.github.com>
2023-12-05 13:41:26 +0300

    Create form.php

---
+++ form.php
@@ -0,0 +1,8 @@
+<form action="ekle.php" method="post">
+    <input type="text" name="name" placeholder="Adı"> <br>
+    <input type="text" name="surname" placeholder="Soyadı"> <br>
+    <input type="text" name="username" placeholder="Kullanıcı Adı"> <br>
+    <input type="text" name="password" placeholder="Şifre"> <br>
+    <input type="text" name="email" placeholder="e-mail"> <br>
+    <input type="submit" value="Kayıt Ol">
+</form>
